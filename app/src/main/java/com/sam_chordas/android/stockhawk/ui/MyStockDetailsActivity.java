82f8cf431576cdf8d01625986764b3961e605051
ahmed_nabil <a_ahmed_nabil@yahoo.com>
2016-09-09 09:16:17 +0200

    -successful use of new yahoo finance api to get stock history -add base code for using it in stock details activity

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStockDetailsActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStockDetailsActivity.java
@@ -18,23 +18,42 @@ public class MyStockDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_graph);
-        lineGraph= (LineChartView) findViewById(R.id.linechart);
-        set=new LineSet();
+        lineGraph = (LineChartView) findViewById(R.id.linechart);
+        set = new LineSet();
 
-        set.addPoint("ff",5);
-        set.addPoint("fd",6);
-        set.addPoint("fgf",8);
-        set.addPoint("fdfgf",1);
-        set.addPoint("ffkj",54);
-        set.addPoint("ffs",89);
-        set.addPoint("ffg",2);
-        set.addPoint("ffza",0);
-        set.addPoint("ffqq",-5);
+        set.addPoint("ff", 5);
+        set.addPoint("fd", 6);
+        set.addPoint("fgf", 8);
+        set.addPoint("fdfgf", 1);
+        set.addPoint("ffkj", 54);
+        set.addPoint("ffs", 89);
+        set.addPoint("ffg", 2);
+        set.addPoint("ffza", 0);
+        set.addPoint("ffqq", -5);
         set.beginAt(0);
         lineGraph.addData(set);
 
         lineGraph.show();
 
+
+//        basic api calling to get stock history and show it
+//        in lineChartView
+        /*
+
+        Calendar from = Calendar.getInstance();
+        Calendar to = Calendar.getInstance();
+        from.add(Calendar.YEAR, -5); // from 5 years ago
+        Stock google = null;
+        try {
+            google = YahooFinance.get("GOOG", from, to, Interval.MONTHLY);
+            Calendar c = google.getHistory().get(0).getDate();
+            String month = String.valueOf(c.get(Calendar.MONTH) + 1);
+            Date d = c.getTime();
+            Log.i("google", "onRunTask: " + google.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+         */
 
     }
 }
